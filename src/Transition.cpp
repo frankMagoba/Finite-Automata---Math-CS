33531adf289c91e185c2527900e28c7ae9905678
frankmag <franklin.magoba@medbookafrica.com>
2020-05-27 13:40:48 +0300

    transition

--- src/Transition.cpp
+++ src/Transition.cpp
@@ -4,20 +4,25 @@
 
 using namespace std;
 
-Transition* Transition::searchByCharacter(vector<Transition*> &list, char c) {
-    vector<Transition*>::iterator it;
-    it = find_if(list.begin(), list.end(), [&c](Transition* tr) -> bool {
+Transition *Transition::searchByCharacter(vector<Transition *> &list, char c)
+{
+    vector<Transition *>::iterator it;
+    it = find_if(list.begin(), list.end(), [&c](Transition *tr) -> bool {
         return tr->trans == c;
     });
-    if (it != list.end()) {
+    if (it != list.end())
+    {
         return *it;
     }
     return nullptr;
 }
 
-void Transition::searchOccurrence(const vector<Transition*> &list, char c, vector<Transition*> &recover) {
-    for (Transition* tr: list) {
-        if (tr->trans == c) {
+void Transition::searchOccurrence(const vector<Transition *> &list, char c, vector<Transition *> &recover)
+{
+    for (Transition *tr : list)
+    {
+        if (tr->trans == c)
+        {
             recover.push_back(tr);
         }
     }
--- src/Transition.h
+++ src/Transition.h
@@ -5,28 +5,27 @@
 
 #define EMPTY '*'
 
-
 // Outgoing transition from a State
-typedef struct Transition {
-    struct State* dest;
+typedef struct Transition
+{
+    struct State *dest;
     char trans;
 
     /// Search transition having the specified character
     /// \param list List of Transition to look in
     /// \param c Character of transition to look for
     /// \return First occurrence, nullptr if none
-    static Transition* searchByCharacter(std::vector<Transition*> &list, char c);
+    static Transition *searchByCharacter(std::vector<Transition *> &list, char c);
 
     /// Recover any occurrences in a list of transition for a character
     /// \param list List of Transition to look in
     /// \param c Character to look for
     /// \param recover List of occurrences (return by parameter)
-    static void searchOccurrence(const std::vector<Transition*> &list, char c, std::vector<Transition*> &recover);
+    static void searchOccurrence(const std::vector<Transition *> &list, char c, std::vector<Transition *> &recover);
 
     /// Sort transition by alphabetical order of character of transition, and if same by ascending order of leading state
     /// \param list List of transitions
-    static void sortTransitions(std::vector<Transition*> &list);
+    static void sortTransitions(std::vector<Transition *> &list);
 } Transition;
-
 
 #endif //FINITE_AUTOMATA___MATH_CS_TRANSITION_H
