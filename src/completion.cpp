1b909f7580ecec266e202670c4e5965ef546851b
frankmag <franklin.magoba@medbookafrica.com>
2020-05-27 13:17:56 +0300

    completion

--- src/completion.cpp
+++ src/completion.cpp
@@ -2,37 +2,44 @@
 #include <vector>
 #include "completion.h"
 
-FA* FA::completion() {
+FA *FA::completion()
+{
     // Normally we only complete Deterministic FA, but since it is technically possible to comple a NDFA, we allow it
-    if (!_completed) {
-        FA* newFA = new FA(*this); // Creates a new FA that will be complete
+    if (!_completed)
+    {
+        FA *newFA = new FA(*this);                           // Creates a new FA that will be complete
         completionProcess(newFA->_states, newFA->_alphabet); // Makes the completion
         newFA->sort();
         newFA->_completed = true;
         newFA->_name = _name + " Complete";
         return newFA;
-    } else {
+    }
+    else
+    {
         return this;
     }
-
 }
 
-static void completionProcess(vector<State*> &list, const vector<char> &alphabet) {
-    State* P = new State; //Declaration of a new state "P"
+static void completionProcess(vector<State *> &list, const vector<char> &alphabet)
+{
+    State *P = new State; //Declaration of a new state "P"
     int i = 0;
-    while (State::searchById(list, to_string(list.size() + i)) != nullptr) {
+    while (State::searchById(list, to_string(list.size() + i)) != nullptr)
+    {
         i++;
     }
     P->id = to_string(list.size() + i);
 
-
     // Declaration of "isPUsed" that will check if the state "P" has been used in this process.
     bool isPUsed = false;
-    for (State* st: list) {
-        for (char c : alphabet) {
+    for (State *st : list)
+    {
+        for (char c : alphabet)
+        {
             //Checking all alphabet for each state, to see if there are empty transitions. If yes, adds P as a transition.
-            Transition* T = new Transition;
-            if (Transition::searchByCharacter(st->exits, c) == nullptr) {
+            Transition *T = new Transition;
+            if (Transition::searchByCharacter(st->exits, c) == nullptr)
+            {
                 T->dest = P;
                 T->trans = c;
                 st->exits.push_back(T);
@@ -41,11 +48,15 @@ static void completionProcess(vector<State*> &list, const vector<char> &alphabet
         }
     }
 
-    if (!isPUsed) {
+    if (!isPUsed)
+    {
         delete P; //If P has not been used, we don't need it, so it is deleted
-    } else {
-        for (char c: alphabet) {
-            Transition* tr = new Transition;
+    }
+    else
+    {
+        for (char c : alphabet)
+        {
+            Transition *tr = new Transition;
             tr->trans = c;
             tr->dest = P;
             P->exits.push_back(tr);
@@ -53,4 +64,3 @@ static void completionProcess(vector<State*> &list, const vector<char> &alphabet
         list.push_back(P); //Adds P to the list of states
     }
 }
-
